d67a330abf96f762a92f6c15404f1bea14442ab1
Syaoran <jinnguyen054@gmail.com>
2024-08-08 18:13:12 +0700

    [ADF-26] Title và Influencer card

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,20 @@
+import InfluencerCard from '@/components/influencer-card';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <div className="space-y-8 py-12 mx-auto max-w-4xl text-center group">
+        <h1 className="text-4xl text-gradient font-bold">Nền tảng Influencer Marketing</h1>
+        <p className="text-muted-foreground">
+          Tìm và thuê những người có ảnh hưởng hàng đầu trên Instagram, TikTok và Youtube để tạo nội dung độc đáo cho
+          thương hiệu của bạn
+        </p>
+      </div>
+      <div className="grid grid-cols-4 gap-6">
+        <InfluencerCard />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/influencer-card.tsx
@@ -0,0 +1,29 @@
+import { formats } from '@/lib/utils';
+import Image from 'next/image';
+
+const InfluencerCard = () => {
+  return (
+    <div className="space-y-2">
+      <div className="relative rounded-lg overflow-hidden group">
+        <Image
+          src="https://scontent.fhan5-2.fna.fbcdn.net/v/t39.30808-6/454530341_523941173319997_4319077826762337608_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHfWjALriNSn9uF5PbBuViLKxA69Vi6aM8rEDr1WLpoz0ymNF_anj5JLNl0fE-pJn6YUTUwSNDo61IOyl9S-6Sf&_nc_ohc=zqj-SlzFHcgQ7kNvgF2Blnc&_nc_ht=scontent.fhan5-2.fna&oh=00_AYCfBS7pXZTUYju-OjYFjO-NrNe3H0TULkwPu_G4d5gLIg&oe=66BA6169"
+          alt="Name"
+          width={400}
+          height={600}
+          className="aspect-thumbnail object-cover transition-transform group-hover:scale-105"
+        />
+        <div className="absolute left-2 bottom-2 text-background">
+          <h6 className="font-semibold">Influencer Name</h6>
+          <span className="text-sm">Đà Nẵng, Việt Nam</span>
+        </div>
+      </div>
+      <div className="flex items-center justify-between">
+        <span className="font-medium text-muted-foreground">Platform</span>
+        <span className="font-bold">{formats.price(500000)}</span>
+      </div>
+      <div>Lifestyle & Fashion content creator</div>
+    </div>
+  );
+};
+
+export default InfluencerCard;
