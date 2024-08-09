3e7e75463f01f0123dbe1b9175f52c5857abd01a
Syaoran <jinnguyen054@gmail.com>
2024-08-09 16:47:15 +0700

    [ADF-26] How AdFusion Work section

---
+++ src/app/(main)/(home)/banner.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button';
+import config from '@/config';
+import Link from 'next/link';
+
+const Banner = () => {
+  return (
+    <div
+      className="bg-no-repeat bg-cover bg-center rounded-3xl overflow-hidden mb-20"
+      style={{
+        backgroundImage: 'url(https://d5ik1gor6xydq.cloudfront.net/websiteImages/creatorMarketplace/cta.png)',
+      }}
+    >
+      <div className="flex flex-col items-center justify-center gap-6 w-full py-20 bg-foreground/50 text-background text-center">
+        <h5 className="text-4xl font-semibold">Tìm và thuê người ảnh hưởng</h5>
+        <p>Tìm kiếm những người có sức ảnh hưởng trên Instagram, TikTok, YouTube.</p>
+        <Button variant="secondary" className="h-16 px-12 text-lg" asChild>
+          <Link href={config.routes.explore}>Bắt đầu tìm kiếm</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 import { LuLoader2 } from 'react-icons/lu';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-all gap-2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-all gap-2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 select-none',
   {
     variants: {
       variant: {
