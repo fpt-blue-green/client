556d78410ac35d9a3d6cd8c516a86df70609f638
Syaoran <jinnguyen054@gmail.com>
2024-08-15 13:49:10 +0700

    [ADF-24] Fix bug

--- src/app/(main)/influencers/[slug]/how-packages-work-modal.tsx
+++ src/app/(main)/influencers/[slug]/how-packages-work-modal.tsx
@@ -1,25 +1,23 @@
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogHeader, DialogTrigger } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { DialogDescription } from '@radix-ui/react-dialog';
 import { LuBadgeInfo } from 'react-icons/lu';
 const HowPackagesWork = () => {
   return (
-    <div className="absolute -bottom-5 left-72">
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button className="absolute right-4 bottom-4 text-muted-foreground" variant={'link'}>
-            <LuBadgeInfo />
-            <span>How does it work?</span>
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px] min-w-[50%] max-h-[85%] ">
-          <DialogHeader className="flex items-center font-semibold text-2xl">
-            Điều gì xảy ra khi tôi giao dịch 1 gói?
-          </DialogHeader>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="text-muted-foreground" variant="link" startIcon={<LuBadgeInfo />}>
+          Hoạt động như thế nào?
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-xl text-center">Điều gì xảy ra khi tôi giao dịch 1 gói?</DialogTitle>
           <DialogDescription></DialogDescription>
-        </DialogContent>
-      </Dialog>
-    </div>
+        </DialogHeader>
+        <div>Content{/* Code content ở đây */}</div>
+      </DialogContent>
+    </Dialog>
   );
 };
 
