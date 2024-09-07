74883009157878dcc909db30a6429a6856ca42db
dinhhieu110 <trandinhhieu11002@gmail.com>
2024-09-07 21:19:45 +0700

    [ADF-87] Image gallery management

---
+++ src/app/(main)/influencers/[slug]/edit/images.tsx
@@ -0,0 +1,129 @@
+'use client';
+import Paper from '@/components/custom/paper';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+import ImageDropdown from './image-dropdown';
+import { UploadIcon } from '@radix-ui/react-icons';
+
+interface IInfluencerImagesModel {
+  id: string;
+  influencerId: string;
+  url: string;
+  description: string;
+  createdAt: string;
+  modifiedAt: string;
+  influencer: string;
+}
+
+const ImageGallery = () => {
+  const handleUploadImage = () => {};
+
+  return (
+    <Paper>
+      <div className="flex justify-between items-center mb-4">
+        <h3 className="font-semibold text-xl">Photos</h3>
+        <div className="flex justify-between items-center">
+          <Button onClick={handleUploadImage} variant="ghost" className="text-purple-500">
+            <UploadIcon />
+            Add photos/videos
+          </Button>
+          <Button variant="ghost"></Button>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-6 gap-4">
+        {influencerImages.map((img) => (
+          <div key={img.id} className="min-w-40 min-h-40 relative">
+            <Image className="w-full rounded-md" src={img.url} width={160} height={160} alt={'User Image'} />
+            <ImageDropdown url={img.url} />
+          </div>
+        ))}
+      </div>
+    </Paper>
+  );
+};
+export default ImageGallery;
+
+const influencerImages: IInfluencerImagesModel[] = [
+  {
+    id: '1',
+    influencerId: '1',
+    url: 'https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/355908271_1770752910006602_459370154146537752_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=585619&_nc_ohc=IsdLUELXHOwQ7kNvgFXKLw9&_nc_ht=scontent.fdad3-5.fna&oh=00_AYAmei6q7z7hTB-F4o3gZ3udfsILopMxoNMU0-Ex5wSaSg&oe=66E21EA8&dl=1',
+    description: 'string',
+    createdAt: '2024-09-07T08:09:31.933Z',
+    modifiedAt: '2024-09-07T08:09:31.933Z',
+    influencer: 'Lucy',
+  },
+  {
+    id: '2',
+    influencerId: '1',
+    url: 'https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/355908271_1770752910006602_459370154146537752_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=585619&_nc_ohc=IsdLUELXHOwQ7kNvgFXKLw9&_nc_ht=scontent.fdad3-5.fna&oh=00_AYAmei6q7z7hTB-F4o3gZ3udfsILopMxoNMU0-Ex5wSaSg&oe=66E21EA8&dl=1',
+    description: 'string',
+    createdAt: '2024-09-07T08:09:31.933Z',
+    modifiedAt: '2024-09-07T08:09:31.933Z',
+    influencer: 'Lucy',
+  },
+  {
+    id: '3',
+    influencerId: '1',
+    url: 'https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/355908271_1770752910006602_459370154146537752_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=585619&_nc_ohc=IsdLUELXHOwQ7kNvgFXKLw9&_nc_ht=scontent.fdad3-5.fna&oh=00_AYAmei6q7z7hTB-F4o3gZ3udfsILopMxoNMU0-Ex5wSaSg&oe=66E21EA8&dl=1',
+    description: 'string',
+    createdAt: '2024-09-07T08:09:31.933Z',
+    modifiedAt: '2024-09-07T08:09:31.933Z',
+    influencer: 'Lucy',
+  },
+  {
+    id: '4',
+    influencerId: '1',
+    url: 'https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/355908271_1770752910006602_459370154146537752_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=585619&_nc_ohc=IsdLUELXHOwQ7kNvgFXKLw9&_nc_ht=scontent.fdad3-5.fna&oh=00_AYAmei6q7z7hTB-F4o3gZ3udfsILopMxoNMU0-Ex5wSaSg&oe=66E21EA8&dl=1',
+    description: 'string',
+    createdAt: '2024-09-07T08:09:31.933Z',
+    modifiedAt: '2024-09-07T08:09:31.933Z',
+    influencer: 'Lucy',
+  },
+  {
+    id: '5',
+    influencerId: '1',
+    url: 'https://danviet.mediacdn.vn/upload/2-2019/images/2019-05-21/Ngam-nhan-sac-cua-hot-girl-Nga-duoc-menh-danh-54522370_1008888162641919_3123301347227598848_n-1558433996-width660height661.jpg',
+    description: 'string',
+    createdAt: '2024-09-07T08:09:31.933Z',
+    modifiedAt: '2024-09-07T08:09:31.933Z',
+    influencer: 'Lucy',
+  },
+  {
+    id: '6',
+    influencerId: '1',
+    url: 'https://danviet.mediacdn.vn/upload/2-2019/images/2019-05-21/Ngam-nhan-sac-cua-hot-girl-Nga-duoc-menh-danh-54522370_1008888162641919_3123301347227598848_n-1558433996-width660height661.jpg',
+    description: 'string',
+    createdAt: '2024-09-07T08:09:31.933Z',
+    modifiedAt: '2024-09-07T08:09:31.933Z',
+    influencer: 'Lucy',
+  },
+  {
+    id: '7',
+    influencerId: '1',
+    url: 'https://danviet.mediacdn.vn/upload/2-2019/images/2019-05-21/Ngam-nhan-sac-cua-hot-girl-Nga-duoc-menh-danh-54522370_1008888162641919_3123301347227598848_n-1558433996-width660height661.jpg',
+    description: 'string',
+    createdAt: '2024-09-07T08:09:31.933Z',
+    modifiedAt: '2024-09-07T08:09:31.933Z',
+    influencer: 'Lucy',
+  },
+  {
+    id: '8',
+    influencerId: '1',
+    url: 'https://danviet.mediacdn.vn/upload/2-2019/images/2019-05-21/Ngam-nhan-sac-cua-hot-girl-Nga-duoc-menh-danh-54522370_1008888162641919_3123301347227598848_n-1558433996-width660height661.jpg',
+    description: 'string',
+    createdAt: '2024-09-07T08:09:31.933Z',
+    modifiedAt: '2024-09-07T08:09:31.933Z',
+    influencer: 'Lucy',
+  },
+  {
+    id: '9',
+    influencerId: '1',
+    url: 'https://danviet.mediacdn.vn/upload/2-2019/images/2019-05-21/Ngam-nhan-sac-cua-hot-girl-Nga-duoc-menh-danh-54522370_1008888162641919_3123301347227598848_n-1558433996-width660height661.jpg',
+    description: 'string',
+    createdAt: '2024-09-07T08:09:31.933Z',
+    modifiedAt: '2024-09-07T08:09:31.933Z',
+    influencer: 'Lucy',
+  },
+];
--- src/app/(main)/influencers/[slug]/edit/page.tsx
+++ src/app/(main)/influencers/[slug]/edit/page.tsx
@@ -3,9 +3,10 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { ArrowLeftIcon, IdCardIcon } from '@radix-ui/react-icons';
 import { getServerSession } from 'next-auth';
 import { FaBox, FaNetworkWired, FaPerson, FaRegImages } from 'react-icons/fa6';
-import Details from './Details';
 import Link from 'next/link';
 import config from '@/config';
+import ImageGallery from './images';
+import Details from './details';
 
 const EditInfluencerProfile = async () => {
   const session = await getServerSession();
@@ -17,35 +18,32 @@ const EditInfluencerProfile = async () => {
           Trở về trang chủ
         </Link>
       </Button>
-      <h1 className="text-2xl font-semibold mt-6 mb-12">Chỉnh Sửa Thông Tin</h1>
+      <h1 className="text-2xl font-semibold mt-6 mb-12 text-center md:text-start">Chỉnh Sửa Thông Tin</h1>
       <Tabs defaultValue="details">
-        <TabsList className="h-13 mb-8">
-          <TabsTrigger value="details" className="py-3 px-10">
+        <TabsList className="h-13 mb-8 flex flex-wrap items-center justify-start">
+          <TabsTrigger value="details" className="py-3 px-14 md:px-10">
             <IdCardIcon className="mr-2" />
             Chi tiết
-          </TabsTrigger>
-          <TabsTrigger value="socialMedias" className="py-3 px-10">
-            <FaNetworkWired className="mr-2" />
-            Mạng xã hội
           </TabsTrigger>
           <TabsTrigger value="images" className="py-3 px-10">
             <FaRegImages className="mr-2" />
             Thư viện ảnh
           </TabsTrigger>
-          <TabsTrigger value="packages" className="py-3 px-10">
+          <TabsTrigger value="socialMedias" className="py-3 px-10">
+            <FaNetworkWired className="mr-2" />
+            Mạng xã hội
+          </TabsTrigger>
+          <TabsTrigger value="packages" className="py-3 px-14 md:px-10">
             <FaBox className="mr-2" />
             Các gói
           </TabsTrigger>
-          <TabsTrigger value="portfolio" className="py-3 px-10">
-            <FaPerson className="mr-2" />
-            Hồ sơ
-          </TabsTrigger>
         </TabsList>
         <TabsContent value="details">{session?.user && <Details user={session.user} />}</TabsContent>
+        <TabsContent value="images">
+          <ImageGallery />
+        </TabsContent>
         <TabsContent value="socialMedias"></TabsContent>
-        <TabsContent value="images"></TabsContent>
         <TabsContent value="packages"></TabsContent>
-        <TabsContent value="portfolio"></TabsContent>
       </Tabs>
     </div>
   );
