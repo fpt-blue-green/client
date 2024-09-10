30ff450844a32fa90b66409a2697e9b5b5cfcd8d
Syaoran <jinnguyen054@gmail.com>
2024-09-10 14:03:23 +0700

    [ADF-0] Fix first mount account

--- src/contexts/auth-provider.tsx
+++ src/contexts/auth-provider.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { FC, ReactNode, useEffect } from 'react';
+import { FC, ReactNode, useEffect, useRef } from 'react';
 import { SessionProvider, signOut, useSession } from 'next-auth/react';
 import { jwtDecode } from 'jwt-decode';
 import { authRequest } from '@/request';
 import config from '@/config';
+import IUser from '@/types/user';
 interface AuthProviderProps {
   children: ReactNode;
 }
@@ -19,35 +20,45 @@ const AuthProvider: FC<Readonly<AuthProviderProps>> = ({ children }) => {
 
 const RefreshProvider: FC<Readonly<AuthProviderProps>> = ({ children }) => {
   const { data: session, update } = useSession();
+  const sessionFlag = useRef(true);
 
-  // Refresh token lỗi thì logout
+  const refreshTokenIfNeeded = async (user: IUser) => {
+    const decoded = jwtDecode(user.accessToken);
+    if (decoded.exp) {
+      // Token còn dưới 2.5 phút thì refresh
+      if (decoded.exp * 1000 - Date.now() < 860_000) {
+        try {
+          const { data } = await authRequest.refreshToken(user.refreshToken);
+          if (data) {
+            const { accessToken, refreshToken } = data;
+            update({
+              ...session,
+              user: {
+                ...session?.user,
+                accessToken,
+                refreshToken,
+              },
+            });
+          }
+        } catch {
+          // Refresh token lỗi thì logout
+          signOut({ callbackUrl: config.routes.login });
+        }
+      }
+    }
+  };
+
   useEffect(() => {
     const user = session?.user;
+
     if (user) {
-      const handler = setInterval(async () => {
-        const decoded = jwtDecode(user.accessToken);
-        if (decoded.exp) {
-          // Token còn dưới 2.5 phút thì refresh
-          if (decoded.exp * 1000 - Date.now() < 150_000) {
-            try {
-              const { data } = await authRequest.refreshToken(user.refreshToken);
-              if (data) {
-                const { accessToken, refreshToken } = data;
-                update({
-                  ...session,
-                  user: {
-                    ...session?.user,
-                    accessToken,
-                    refreshToken,
-                  },
-                });
-              }
-            } catch {
-              signOut({ callbackUrl: config.routes.login });
-            }
-          }
-        }
-      }, 60_000);
+      // refresh token khi user được lưu trong cookie và app được chạy lần đầu
+      if (sessionFlag.current) {
+        refreshTokenIfNeeded(user);
+        sessionFlag.current = false;
+      }
+
+      const handler = setInterval(() => refreshTokenIfNeeded(user), 60_000);
 
       return () => clearInterval(handler);
     }
