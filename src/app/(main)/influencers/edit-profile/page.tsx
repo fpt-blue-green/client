117fc0eec66d7258a692db5ec04946eb8e2ec693
dinhhieu110 <trandinhhieu11002@gmail.com>
2024-09-10 23:51:48 +0700

    [ADF-87] Edit page - Tab 4 - Packages

--- src/app/(main)/influencers/edit-profile/page.tsx
+++ src/app/(main)/influencers/edit-profile/page.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import config from '@/config';
 import ImageGallery from './images';
 import Details from './details';
-// import Packages from './packages';
+import Packages from './packages';
 
 const EditInfluencerProfile = async () => {
   const session = await getServerSession();
@@ -43,7 +43,9 @@ const EditInfluencerProfile = async () => {
           <ImageGallery />
         </TabsContent>
         <TabsContent value="socialMedias"></TabsContent>
-        <TabsContent value="packages">{/* <Packages /> */}</TabsContent>
+        <TabsContent value="packages">
+          <Packages />
+        </TabsContent>
       </Tabs>
     </div>
   );
