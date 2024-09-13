2bd6bae1b27cbeaab5d21378c2e7b614ab6ccd7f
dinhhieu110 <trandinhhieu11002@gmail.com>
2024-09-14 02:34:19 +0700

    [ADF-87] Handle APIs - Edit Details & Upload Avatar

--- src/app/(main)/influencers/edit-profile/details.tsx
+++ src/app/(main)/influencers/edit-profile/details.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import AvatarUploader from '@/components/avatar-uploader';
 import Paper from '@/components/custom/paper';
 import { Input } from '@/components/ui/input';
@@ -8,33 +8,68 @@ import { Label } from '@/components/ui/label';
 import IUser from '@/types/user';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { GeneralBodyType, generalSchema } from '@/schema-validations/influencer.schema';
+import { generalSchema } from '@/schema-validations/influencer.schema';
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import { Button } from '@/components/ui/button';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { EGender } from '@/types/enum';
 import { Textarea } from '@/components/ui/textarea';
-
+import IInfluencer from '@/types/influencer';
+import { avatarSchema } from '@/schema-validations/user.schema';
+import { z } from 'zod';
+import { useSession } from 'next-auth/react';
+import { influencerRequest } from '@/request';
+import { toast } from 'sonner';
 interface GeneralProps {
   user: IUser;
+  influencer: IInfluencer;
 }
 
-const Details: FC<GeneralProps> = ({ user }) => {
-  const form = useForm<GeneralBodyType>({
-    resolver: zodResolver(generalSchema),
+const Details: FC<GeneralProps> = ({ user, influencer }) => {
+  const { data: session, update } = useSession();
+  const [loading, setLoading] = useState(false);
+  const combinedSchema = generalSchema.merge(avatarSchema);
+  const { data } = useSession<true>();
+
+  type CombinedFormType = z.infer<typeof combinedSchema>;
+  const form = useForm<CombinedFormType>({
+    resolver: zodResolver(combinedSchema),
     defaultValues: {
-      name: user.name,
-      summarize: '',
-      description: '',
-      address: '',
-      gender: EGender.Male,
+      fullName: influencer.fullName || '',
+      summarise: influencer.summarise || '',
+      description: influencer.description || '',
+      address: influencer.address || '',
+      gender: influencer.gender || EGender.Male,
+      slug: influencer.slug || '',
     },
   });
 
-  const onSubmit = (values: GeneralBodyType) => {
-    console.log(values);
-  };
+  const avatarRef = form.register('avatar');
 
+  const onSubmit = async (values: CombinedFormType) => {
+    setLoading(true);
+    try {
+      await influencerRequest.updateGeneralInfo(values);
+      const avatar = values.avatar[0];
+      if (avatar) {
+        const avatarRes = await influencerRequest.changeAvatar(avatar);
+        if (avatarRes.data) {
+          await update({
+            ...session,
+            user: {
+              ...session?.user,
+              image: avatarRes.data,
+            },
+          });
+        }
+      }
+      toast.success('Cập nhật thông tin thành công.');
+    } catch (err: any) {
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-1 lg:grid-cols-3 gap-6">
@@ -46,7 +81,7 @@ const Details: FC<GeneralProps> = ({ user }) => {
               render={() => (
                 <FormItem className="flex flex-col items-center justify-center gap-4 my-auto">
                   <FormControl>
-                    <AvatarUploader defaultSrc={user.image} fallback={user.name[0]} />
+                    <AvatarUploader defaultSrc={influencer.avatar} {...avatarRef} />
                   </FormControl>
                   <FormMessage />
                   <p className="text-xs text-muted-foreground text-center">
@@ -63,11 +98,11 @@ const Details: FC<GeneralProps> = ({ user }) => {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-8">
             <div className="space-y-2">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" className="w-full" type="email" defaultValue={user.email} readOnly disabled />
+              <Input id="email" className="w-full" type="email" defaultValue={data?.user.email} readOnly disabled />
             </div>
             <FormField
               control={form.control}
-              name="name"
+              name="fullName"
               render={({ field }) => (
                 <FormItem>
                   <Label htmlFor="name">Tên</Label>
@@ -105,7 +140,7 @@ const Details: FC<GeneralProps> = ({ user }) => {
                       <SelectTrigger className="w-full">
                         <SelectValue placeholder="Giới tính" />
                       </SelectTrigger>
-                      <SelectContent>
+                      <SelectContent defaultValue={influencer.gender}>
                         <SelectItem value={EGender.Male.toString()}>Nam</SelectItem>
                         <SelectItem value={EGender.Female.toString()}>Nữ</SelectItem>
                         <SelectItem value={EGender.Others.toString()}>Khác</SelectItem>
@@ -118,7 +153,7 @@ const Details: FC<GeneralProps> = ({ user }) => {
             />
             <FormField
               control={form.control}
-              name="summarize"
+              name="summarise"
               render={({ field }) => (
                 <FormItem className="md:col-span-2">
                   <Label htmlFor="summarize">Tóm tắt</Label>
@@ -144,7 +179,9 @@ const Details: FC<GeneralProps> = ({ user }) => {
             />
           </div>
           <div className="mt-8 text-right">
-            <Button type="submit">Lưu thay đổi</Button>
+            <Button type="submit" size="large" variant="gradient" loading={loading}>
+              Lưu thay đổi
+            </Button>
           </div>
         </Paper>
       </form>
