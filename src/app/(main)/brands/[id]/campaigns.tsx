f2b56ddd3dfdb9a040055568c5a62887c979cb1d
dinhhieu110 <trandinhhieu11002@gmail.com>
2024-10-09 01:56:03 +0700

    [ADF-131] Brand Details Page - Campaigns sections

---
+++ src/app/(main)/brands/[id]/campaigns.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { fetcher } from '@/lib/http';
+import ICampaign from '@/types/campaign';
+import Image from 'next/image';
+import Link from 'next/link';
+import useSWRImmutable from 'swr/immutable';
+
+const Campaigns = () => {
+  const { data: campaigns, isLoading } = useSWRImmutable<ICampaign[]>('/Brand/campaigns', fetcher);
+  return (
+    <div className="mb-10">
+      <h2 className="mt-10 mb-4 text-xl font-semibold">Chiến dịch của bạn</h2>
+      <div className="grid grid-cols-2 gap-4">
+        {campaigns &&
+          campaigns.map((item, index) => (
+            <div key={index} className="w-full relative">
+              <Link href={''} target="_blank">
+                <Image
+                  className="w-full object-cover rounded-sm ease-out duration-300"
+                  src={
+                    item.images[0]?.url ||
+                    'https://d5ik1gor6xydq.cloudfront.net/websiteImages/creatorMarketplace/noPic.png'
+                  }
+                  alt={item.name}
+                  width={400}
+                  height={200}
+                />
+                <p className="absolute bottom-2 left-2 text-primary-foreground">{item.title || ''}</p>
+              </Link>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Campaigns;
