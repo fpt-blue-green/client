ff45d673961763f468ce474c49c30891aefc1fb1
dinhhieu110 <trandinhhieu11002@gmail.com>
2024-10-10 00:49:30 +0700

    [ADF-127] Edit Brand Profile - 3 tabs

--- src/app/(main)/brands/edit-profile/images.tsx
+++ src/app/(main)/brands/edit-profile/images.tsx
@@ -1,120 +1,85 @@
 'use client';
 
 import Paper from '@/components/custom/paper';
-import { ChangeEvent, FC, useRef, useState } from 'react';
+import { FC, useState } from 'react';
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import AvatarUploader from '@/components/avatar-uploader';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { AvatarBody, avatarSchema } from '@/schema-validations/user.schema';
 import { Button } from '@/components/ui/button';
-import { userRequest } from '@/request';
+import { brandRequest, userRequest } from '@/request';
 import { useSession } from 'next-auth/react';
 import { toast } from 'sonner';
-import Image from 'next/image';
-import { Input } from '@/components/ui/input';
 import { RiEditFill } from 'react-icons/ri';
 import BrandDetailsProps from './props';
+import { ImagesBodyType, imagesSchema } from '@/schema-validations/brand.schema';
+import CoverUploader from '@/components/cover-uploader';
 
 const Images: FC<BrandDetailsProps> = ({ brand, mutate }) => {
   const { data: session, update } = useSession();
-  const fileInputRef = useRef<HTMLInputElement>(null);
-  const [backgroundImage, setBackgroundImage] = useState();
-  const [imageFile, setImageFile] = useState<File[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  const [loadingAvatar, setLoadingAvatar] = useState<boolean>(false);
 
-  const avatarForm = useForm<AvatarBody>({
-    resolver: zodResolver(avatarSchema),
+  const form = useForm<ImagesBodyType>({
+    resolver: zodResolver(imagesSchema),
   });
-  const avatarRef = avatarForm.register('avatar');
 
-  const handleUploadBackgroundImg = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
+  const avatarRef = form.register('avatar');
+  const coverRef = form.register('cover');
 
-  const onSubmitAvatar = async (values: AvatarBody) => {
-    const avatar = values.avatar?.[0] as File;
-    setLoadingAvatar(true);
-    try {
-      const res = await userRequest.changeAvatar(avatar);
-      if (res.data) {
-        await update({
-          ...session,
-          user: {
-            ...session?.user,
-            image: res.data,
-          },
-        });
-        // mutate().then(() => toast.success('Cập nhật ảnh đại diện thành công'));
-      }
-    } catch (err: any) {
-      toast.error(err.message);
-    } finally {
-      setLoadingAvatar(false);
-    }
-  };
-
-  const handleOnChangeImage = (event: ChangeEvent<HTMLInputElement>) => {};
-
-  const handleOnBackgroundImgClick = () => {
-    fileInputRef.current?.click();
-  };
-
-  const handleSaveChanges = () => {
+  const onSubmit = (values: ImagesBodyType) => {
     setLoading(true);
-    // Call API
-    //     .then(() => {
-    //       mutate().then(() => {
-    //         toast.success('Bạn cập nhật thư viện ảnh thành công');
-    //       });
-    //     })
-    //     .catch((err) => toast.error(err.message))
-    //     .finally(() => setLoading(false));
+    const uploader: Promise<any>[] = [
+      values.avatar[0] ? userRequest.changeAvatar(values.avatar[0] as File) : Promise.resolve(),
+    ];
+    if (values.cover.length > 0) {
+      uploader.push(brandRequest.changeCover(values.cover[0] as File));
+    }
+
+    Promise.all(uploader)
+      .then(([res]) => {
+        if (res) {
+          update({
+            ...session,
+            user: {
+              ...session?.user,
+              image: res.data,
+            },
+          });
+        }
+        mutate().then(() => toast.success('Cập nhật ảnh thành công'));
+      })
+      .catch((err) => toast.error(err.message))
+      .finally(() => setLoading(false));
   };
+
+  const onError = (err: any) => {
+    toast.error(err.cover.message);
+  };
+
   return (
     <Paper>
       <div>
-        <Form {...avatarForm}>
-          <form onSubmit={avatarForm.handleSubmit(onSubmitAvatar)}>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit, onError)}>
             <div className="flex flex-col gap-10 mb-8">
               <FormField
-                control={avatarForm.control}
+                control={form.control}
                 name="avatar"
                 render={() => (
                   <FormItem className="flex flex-col items-center justify-center gap-4 my-auto">
                     <FormControl>
-                      <AvatarUploader
-                        defaultSrc="https://img.freepik.com/premium-photo/boy-with-anime-character-his-hoodie_662214-103222.jpg"
-                        {...avatarRef}
-                      />
+                      <AvatarUploader defaultSrc={brand.avatar} {...avatarRef} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
               <FormField
-                control={avatarForm.control}
+                control={form.control}
                 name="avatar"
                 render={() => (
                   <div className="col-span-2 relative">
-                    <Image
-                      className="w-full max-h-[480px] object-cover cursor-pointer hover:opacity-80 rounded-md"
-                      src="https://d5ik1gor6xydq.cloudfront.net/buyers/194517/17268433977072098.webp"
-                      alt="Brand_Avatar"
-                      width={800}
-                      height={400}
-                      onClick={handleOnBackgroundImgClick}
-                    />
-                    <Input
-                      type="file"
-                      accept="image/jpeg,image/png,image/gif"
-                      className="hidden"
-                      ref={fileInputRef}
-                      onChange={handleOnChangeImage}
-                    />
+                    <CoverUploader {...coverRef} defaultSrc={brand.coverImg} />
                     <div className="absolute top-2 left-2 bg-primary-foreground px-3 rounded-sm text-foreground">
                       <p>Ảnh bìa</p>
                     </div>
@@ -123,13 +88,7 @@ const Images: FC<BrandDetailsProps> = ({ brand, mutate }) => {
               />
             </div>
             <div className="flex justify-end">
-              <Button
-                onClick={handleSaveChanges}
-                size="large"
-                variant="gradient"
-                className="font-bold h-12"
-                loading={loading}
-              >
+              <Button type="submit" size="large" variant="gradient" className="font-bold h-12" loading={loading}>
                 <RiEditFill />
                 Lưu thay đổi
               </Button>
