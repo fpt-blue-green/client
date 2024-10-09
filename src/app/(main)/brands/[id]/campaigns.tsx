dab736d647c3684d8b66d139424df60c59953cff
dinhhieu110 <trandinhhieu11002@gmail.com>
2024-10-10 00:47:44 +0700

    [ADF-131] Brand details page

--- src/app/(main)/brands/[id]/campaigns.tsx
+++ src/app/(main)/brands/[id]/campaigns.tsx
@@ -6,13 +6,13 @@ import Link from 'next/link';
 import useSWRImmutable from 'swr/immutable';
 
 const Campaigns = () => {
-  const { data: campaigns, isLoading } = useSWRImmutable<ICampaign[]>('/Brand/campaigns', fetcher);
+  const { data: campaigns } = useSWRImmutable<ICampaign[]>('/Brand/campaigns', fetcher);
   return (
     <div className="mb-10">
       <h2 className="mt-10 mb-4 text-xl font-semibold">Chiến dịch của bạn</h2>
-      <div className="grid grid-cols-2 gap-4">
-        {campaigns &&
-          campaigns.map((item, index) => (
+      {campaigns && campaigns.length > 0 ? (
+        <div className="grid grid-cols-2 gap-4">
+          {campaigns.map((item, index) => (
             <div key={index} className="w-full relative">
               <Link href={''} target="_blank">
                 <Image
@@ -29,7 +29,10 @@ const Campaigns = () => {
               </Link>
             </div>
           ))}
-      </div>
+        </div>
+      ) : (
+        <p className="text-sm font-light">Bạn chưa có chiến dịch nào.</p>
+      )}
     </div>
   );
 };
