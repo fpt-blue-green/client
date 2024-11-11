6e71a20e6e9f1f30d55e654998fb9f4d0359dac2
Syaoran <jinnguyen054@gmail.com>
2024-11-11 11:29:58 +0700

    [ADF-296] Account payment UI

--- src/app/(main)/account/page.tsx
+++ src/app/(main)/account/page.tsx
@@ -1,25 +1,43 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { LockClosedIcon } from '@radix-ui/react-icons';
+import { LuCreditCard } from 'react-icons/lu';
 import Security from './security';
-import { getServerSession } from 'next-auth';
+import Payment from './payment';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Tài khoản',
+  description: 'Quản lý tài khoản của riêng bạn',
+};
 
 const Account = async () => {
-  const session = await getServerSession();
-
   return (
     <div className="container my-8">
       <h1 className="text-2xl font-semibold mb-7">Tài khoản</h1>
       <Tabs defaultValue="security">
-        <TabsList className="h-13 mb-8">
-          <TabsTrigger value="security" className="py-3 px-10">
+        <TabsList className="mb-8">
+          <TabsTrigger value="security" className="px-8">
             <LockClosedIcon className="mr-2" />
             Bảo mật
           </TabsTrigger>
+          <TabsTrigger value="payment" className="px-8">
+            <LuCreditCard className="mr-2" />
+            Thanh toán
+          </TabsTrigger>
         </TabsList>
-        <TabsContent value="security">{session?.user && <Security />}</TabsContent>
+        <TabsContent value="security">
+          <Security />
+        </TabsContent>
+        <TabsContent value="payment">
+          <Payment />
+        </TabsContent>
       </Tabs>
     </div>
   );
 };
+
+export function generateStaticParams() {
+  return [{ role: 'influencer' }, { role: 'brand' }];
+}
 
 export default Account;
