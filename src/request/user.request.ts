fac1a91b4d0ccae3ba491d3b43792217ff867bb9
Syaoran Nguyen <jinnguyen054@gmail.com>
2024-11-13 00:51:35 +0700

    [ADF-296] Add money

--- src/request/user.request.ts
+++ src/request/user.request.ts
@@ -1,5 +1,6 @@
 import http from '@/lib/http';
 import { ChangePasswordBodyType } from '@/schema-validations/user.schema';
+import { IPaymentResponse } from '@/types/payment';
 
 const userRequest = {
   changePassword: (body: ChangePasswordBodyType) => http.put('/Auth/changePass', body),
@@ -8,6 +9,8 @@ const userRequest = {
     formData.append('file', avatar);
     return http.patch<string>('/User/avatar', formData);
   },
+  deposit: (redirectUrl: string, amount: number) =>
+    http.post<IPaymentResponse>('/Payment/deposit/CreateCollectionLink', { redirectUrl, amount }),
 };
 
 export default userRequest;
